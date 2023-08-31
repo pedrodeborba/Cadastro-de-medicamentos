2119543f93117be7140943d3abd26cc4b6e98383
Pedro de Borba <pedrodeborba1709@gmail.com>
2023-08-31 19:43:46 -0300

    :art: create config.json

--- models/db.js
+++ models/db.js
@@ -1,5 +1,6 @@
 const Sequelize = require ('sequelize');
-const sequelize = new Sequelize('mydb', 'root', '12345678', {
+
+const sequelize = new Sequelize('mydb','root','12345678', {
     host: "localhost",
     dialect: 'mysql'
 });
