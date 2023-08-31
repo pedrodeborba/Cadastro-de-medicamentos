1ba8ccf32837c0590a50083f407dbfcf749c3f63
Pedro de Borba <pedrodeborba1709@gmail.com>
2023-08-31 19:50:50 -0300

    :sparkles: add jsonwebtoken

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,22 +1,48 @@
 const usuario = require("../models/usuarioModel");
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const config = require('../config/config.json');
 
 function getLogin(req, res) {
     res.render('login', { erro: null});
 }
 
-function autenticar(req, res) {
+
+const buildtoken = async (id) => {
+    if (id > 0) {
+      return jwt.sign({ id }, config.token, { expiresIn: 43200 });
+    }
+    return false;
+};
+
+const validarToken = async (token) => jwt.verify(token, config.keys.token, (err) => {
+    if (err) {
+      return false;
+    }
+    return true;
+});
+  
+async function autenticar(req, res) {
     const email = req.body.email;
-    const senha = req.body.senha;
+    let senha = req.body.senha;
 
     if (!email || !senha) {
         // Campos vazios, renderizar página de login com mensagem de erro
         res.render('login', { content: 'login', erro: 'Preencha todos os campos' });
     }
 
+    senha = await crypto.createHash('md5').update(senha).digest('hex');
+
     usuario.autenticar(email, senha)
         .then(user => {
             if (user) {
-                // Usuário autenticado, redirecionar para a tela de home
+
+                const token = buildtoken(user.id);
+
+                // ARMAZENAR O TOKEN NO LOCALSTORAGE
+                // Usar ele no cabecalho da requisicao de criar e listar medicamentos
+                // Usar a função validarToken PARA VERIFICAR SE O TOKEN É VALIDO, ela ta ali em cima 
+
                 res.redirect('/home');
             } else {
                 // Credenciais inválidas, renderizar página de login com mensagem de erro
